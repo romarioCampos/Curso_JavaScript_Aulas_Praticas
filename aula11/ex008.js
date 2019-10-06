6f2de93436366e2ae72af6369a5f4fe3ed727fd8
Romário Campos <oliveira-campos@outlook.com.br>
2019-10-05 21:16:07 -0300

    Exercicios praticos sobre JavaScript, HTML E CSS

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 78.2
+console.log(`A velocidade do seu carro é ${velocidade}Km/h`)
+if (velocidade > 60) { //Condição simples
+    console.log(`Você ultrapassaou a velocidade permitida. Multado!`)
+}
+console.log(`Diriga sempre usando cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`Vivendo em país ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Você é Brasileiro!')
+} else {
+    console.log('Você é Estrangeiro!')
+}
